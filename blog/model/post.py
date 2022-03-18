3b7cbfd39db587ddf3885960a3b9032302333a5c
Renat Yuldashev <renatyv@gmail.com>
2022-03-18 15:29:52 +0300

    Use pydantic for posts. Remove unused method

--- blog/model/post.py
+++ blog/model/post.py
@@ -1,29 +1,40 @@
-import sqlite3
+from typing import Optional
 
-posts = [{"post_id": 0,
-          "title": "Huyak Huyak",
-          "user_id": 0,
-          "text": "Deploy"},
-         {"post_id": 1,
-          "title": "Who is the the best?",
-          "user_id": 0,
-          "text": "Renat!"},
-         {"post_id": 2,
-          "user_id": 2,
-          "title": "Why?",
-          "text": "Because"}
-         ]
+from pydantic import BaseModel
+
+posts_data = [{"post_id": 0,
+               "title": "Huyak Huyak",
+               "user_id": 0,
+               "text": "Deploy"},
+              {"post_id": 1,
+               "title": "Who is the the best?",
+               "user_id": 0,
+               "text": "Renat!"},
+              {"post_id": 2,
+               "user_id": 2,
+               "title": "Why?",
+               "text": "Because"}
+              ]
 
 
-def get_all_posts():
+class Post(BaseModel):
+    post_id: int
+    user_id: int
+    title: str
+    text: str
+
+
+posts = [Post(**post) for post in posts_data]
+
+
+def get_all_posts() -> list[Post]:
     return posts
 
 
-def get_post_by_user_id(user_id: int):
-    """filter posts by user_id"""
-    return [post for post in posts if post["user_id"] == user_id]
-
-
-def get_post_by_post_id(post_id: int):
+def get_post_by_post_id(post_id: int) -> Optional[Post]:
     """filter posts by post_id"""
-    return [post for post in posts if post["post_id"] == post_id]
+    found_posts = [post for post in posts if post.post_id == post_id]
+    if found_posts:
+        return found_posts[0]
+    else:
+        return None
