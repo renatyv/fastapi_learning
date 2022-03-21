c937378d4ec666a9b9a3b132ad35e285c2f13719
Renat Yuldashev <renatyv@gmail.com>
2022-03-21 09:58:01 +0300

    Add test data to blog_user table

---
+++ db_migrations/blog_002_add_initial_users.rollback.sql
@@ -0,0 +1,6 @@
+DELETE FROM
+            blog_user
+WHERE
+      (name='Ibigdan' AND surname='Pasha')
+   OR (name='Vladimir' AND surname='Putin')
+   OR (name='Nikolay' AND surname='Sergeev');
---
+++ db_migrations/blog_002_add_initial_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO blog_user(name,surname)
+VALUES ('Ibigdan', 'Pasha'),
+       ('Vladimir', 'Putin'),
+       ('Nikolay', 'Sergeev');
