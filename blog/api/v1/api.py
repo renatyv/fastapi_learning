ee93d1f3739e73c6d70a4fd617cc924b0eadacd3
Renat Yuldashev <renatyv@gmail.com>
2022-03-27 10:38:40 +0300

    Introduce API versioning. Read database settings from the environment using Pydantic BaseSettings.

---
+++ blog/api/v1/api.py
@@ -0,0 +1,94 @@
+from typing import Union, Optional, Any
+
+from fastapi import FastAPI, Query, Path, Body, HTTPException, Depends, APIRouter
+from sqlalchemy.engine import Connection
+from blog.http_status_codes import HttpStatusCode
+
+from blog import database
+from blog.model import user
+from blog.model import post
+from loguru import logger
+
+api_router = APIRouter()
+
+
+@api_router.get("/users", status_code=HttpStatusCode.OK.value, response_model=list[user.User])
+def users(skip: int = Query(0, ge=0.0, example=2),
+          limit: int = 10,
+          db_connection: Connection = Depends(database.get_database_connection)) -> Any:
+    """get all users
+    :param skip. skip >= 0. optional
+    :param limit. default 10. optional
+    """
+    return user.get_all_users(db_connection, skip=skip, limit=limit)
+
+
+@api_router.get("/users/{user_id}", status_code=HttpStatusCode.FOUND.value, response_model=Optional[user.User])
+def get_user(user_id: int = Path(..., ge=0.0),
+             db_connection: Connection = Depends(database.get_database_connection)) -> Optional[user.User]:
+    """get specific user
+    :param user_id is required, greater than 0
+    """
+    found_user = user.get_user_by_id(user_id, db_connection)
+    if found_user:
+        return found_user
+    else:
+        raise HTTPException(status_code=HttpStatusCode.NOT_FOUND.value)
+
+
+@api_router.post("/users/",status_code=HttpStatusCode.OK.value)
+def create_user(username: str = Body(..., # required, no default value. = None to make optional
+                                     min_length=1,
+                                     max_length=10,
+                                     regex='\w'),
+                surname: str = Body(..., min_length=1,
+                                     max_length=10,
+                                     regex='\w'),
+                db_connection: Connection = Depends(database.get_database_connection)):
+    try:
+        return user.create_user(username, surname,db_connection)
+    except user.DuplicateUserCreationException as e:
+        logger.info('Trying to create duplicate user:'+str(e))
+        raise HTTPException(status_code=HttpStatusCode.CONFLICT.value,
+                            detail=str(e))
+
+
+
+@api_router.get("/posts", status_code=HttpStatusCode.OK.value, response_model=list[post.Post])
+def posts(skip: int = Query(0, ge=0.0, example=2),
+          limit: int = 10,
+          db_connection: Connection = Depends(database.get_database_connection)):
+    """get all posts"""
+    return post.get_all_posts(db_connection, skip=skip, limit=limit)
+
+
+@api_router.get("/posts/{post_id}", status_code=HttpStatusCode.FOUND.value, response_model=post.Post)
+def get_post(post_id: int = Path(..., ge=0.0),  # required, no default value. = None to make optional
+             db_connection: Connection = Depends(database.get_database_connection)):
+    """get specific post
+    :param post_id path param. post_id >= 0. required.
+    """
+    found_post = post.get_post_by_post_id(post_id,db_connection)
+    if found_post:
+        return found_post
+    else:
+        raise HTTPException(status_code=HttpStatusCode.NOT_FOUND.value)
+
+
+@api_router.post("/posts/", status_code=HttpStatusCode.CREATED.value, response_model=post.Post)
+def create_post(user_id: int = Body(..., ge=0),  # required, no default value. = None to make optional
+                title: str = Body(..., min_length=1,
+                                     max_length=300),
+                body: str = Body(..., min_length=0,
+                                  max_length=10000),
+                db_connection: Connection = Depends(database.get_database_connection)) -> post.Post:
+    """create new post
+    :param user_id: may exist in users. If it does not, no error is returned, post is created"""
+    try:
+        return post.create_post(user_id, title, body, db_connection)
+    except post.NoSuchUseridException as e:
+        logger.debug(str(e))
+        raise HTTPException(status_code=HttpStatusCode.NOT_ACCEPTABLE.value, detail='No user with such user_id')
+    except Exception as e:
+        logger.error(str(e))
+        raise HTTPException(status_code=HttpStatusCode.CONFLICT.value, detail='Unknown error')
--- blog/database.py
+++ blog/database.py
@@ -1,30 +1,30 @@
 from typing import Generator
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, validator, Field
 
 import sqlalchemy
-from loguru import logger
-import os
-
-REQUIRED_ENV_VARIABLES = ['PSQL_URL','PSQL_USER', 'PSQL_PASSWORD', 'PSQL_DB', 'LOG_LEVEL']
-
-for env_var in REQUIRED_ENV_VARIABLES:
-    if env_var not in os.environ:
-        logger.error(f"Environment variable {env_var} is not set")
 
 
-logger.info("psql url:"+f"postgresql://"
-                        f"{os.environ['PSQL_USER']}:"
-                        f"PSQL_PASSWORD"
-                        f"@{os.environ['PSQL_URL']}/{os.environ['PSQL_DB']}")
+class DatabaseSettings(BaseSettings):
+    """ Pydantic will read and validate DB connection parameters from the environment variables"""
+    PSQL_USER: str
+    PSQL_PASSWORD: str
+    PSQL_DB: str
+    LOG_LEVEL: str
+    PSQL_URL: str
+
+
+db_settings = DatabaseSettings()
 
 
 engine = sqlalchemy.create_engine(f"postgresql://"
-                                  f"{os.environ['PSQL_USER']}:"
-                                  f"{os.environ['PSQL_PASSWORD']}"
-                                  f"@{os.environ['PSQL_URL']}/{os.environ['PSQL_DB']}")
+                                  f"{db_settings.PSQL_USER}:"
+                                  f"{db_settings.PSQL_PASSWORD}"
+                                  f"@{db_settings.PSQL_URL}/{db_settings.PSQL_DB}")
 
 
 def get_database_connection() -> Generator:
-    connection = engine.connect() # get a connection from pool
+    """get a connection from pool"""
+    connection = engine.connect()
     try:
         yield connection
     finally:
