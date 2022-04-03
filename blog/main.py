f31e4facbf60e9cba24ee1c7436db6f508f5b850
Renat Yuldashev <renatyv@gmail.com>
2022-04-03 11:55:48 +0300

    Using intercepting all uvicorn logs with loguru is too verbose. Only use loguru for my own app's logs.

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,7 @@
-from typing import Union, Optional, Any
-
 from fastapi import FastAPI
 from blog.api.v1 import api as api_v1
-import blog.logger_config
 
 app = FastAPI()
-
-# Force uvicorn to emit logs using the same loguru configs
-blog.logger_config.setup_logging()
 
 
 # connect V1 API using prefix
