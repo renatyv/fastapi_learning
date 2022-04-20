21f18cc2a620385427dc1dbc1421f705c21255f8
Renat Yuldashev <renatyv@gmail.com>
2022-04-20 12:38:10 +0300

    Use Poetry instead of requirements.txt

--- start_debug.sh
+++ start_debug.sh
@@ -1,6 +1,3 @@
-echo 'activating venv'
-source venv/bin/activate
-
 echo "Running code style checks"
 flake8 blog/ --max-line-length 120  --statistics
 flake8 tests/ --max-line-length 120  --statistics
@@ -10,7 +7,7 @@ echo 'running unit tests'
 # python3 -m is used instead of 'pytest tests/' to include source directory into python path
 #--tb=no, don't show long code parts if exception raised when running test (no tracebacks)
 #--asyncio-mode=strict all async tests and fixtures must be marketed with pytest_asyncio
-python3 -m pytest --no-header --tb=no --asyncio-mode=strict tests/
+poetry run pytest --no-header --tb=no --asyncio-mode=strict tests/
 
 echo 'building containers'
 docker-compose -f docker-compose.yml -f config/docker-compose-debug.yml build
@@ -22,7 +19,7 @@ echo 'sleeping for 2s before starting api tests'
 sleep 2
 
 echo 'Check that API is alive'
-python3 -m pytest --no-header --tb=no tests_api/
+poetry run pytest --no-header --tb=no tests_api/
 
 echo 'connecting to the containers to see logs'
 docker-compose -f docker-compose.yml -f config/docker-compose-debug.yml logs -f
