29391f60699dcc3f98716a8b7c5a8efeb3f1e373
Renat Yuldashev <renatyv@gmail.com>
2022-05-08 14:09:49 +0300

    Debug script improvements: 1. Remove redundant re-run of unit tests. 2. Dismantle containers if they were not properly started with docker-compose up.

--- start_debug.sh
+++ start_debug.sh
@@ -24,22 +24,15 @@ fi
 echo 'starting database & web api in docker'
 docker-compose -f docker-compose.yml -f config/docker-compose-debug.yml up -d
 if [[ $? -ne 0 ]] ; then
-    exit 1
+  docker-compose -f docker-compose.yml -f config/docker-compose-debug.yml down -d
+  exit 1
 fi
 
 echo 'sleeping for 2s before starting api tests'
 sleep 2
 
-echo 'Check that API is alive'
+echo 'Runnint API tests'
 poetry run pytest --no-header --tb=no tests_api/
-echo 'running unit tests'
-# python3 -m is used instead of 'pytest tests/' to include source directory into python path
-#--tb=no, don't show long code parts if exception raised when running test (no tracebacks)
-#--asyncio-mode=strict all async tests and fixtures must be marketed with pytest_asyncio
-poetry run pytest --no-header --tb=no --asyncio-mode=strict tests/
-if [[ $? -ne 0 ]] ; then
-    exit 1
-fi
 
 echo 'connecting to the containers to see logs'
 docker-compose -f docker-compose.yml -f config/docker-compose-debug.yml logs -f
